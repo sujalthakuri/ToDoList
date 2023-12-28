485ae1d098111c32db94b1647d52c7aee27d6a21
Sujal Singh Thakuri <sujalthakuribe@gmail.com>
2023-12-28 10:34:15 +0545

    Input Keyboard and Button Added

--- App.tsx
+++ App.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
-import {View, Button, StyleSheet, Text} from 'react-native';
+import {
+  View,
+  Button,
+  StyleSheet,
+  Text,
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
 import Task from './components/Task';
 
 const App = () => {
@@ -12,6 +21,16 @@ const App = () => {
           <Task text="Task 2" />
         </View>
       </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.writeTaskWrapper}>
+        <TextInput style={styles.input} placeholder={'Write a Task'} />
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 };
@@ -32,5 +51,34 @@ const styles = StyleSheet.create({
   items: {
     marginTop: 30,
   },
+  writeTaskWrapper: {
+    padding: 20,
+    justifyContent: 'space-between',
+    position: 'absolute',
+    bottom: 60,
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+  },
+  input: {
+    backgroundColor: '#FFF',
+    padding: 10,
+    width: 250,
+    alignContent: 'center',
+    borderColor: '#C0C0C0',
+    borderRadius: 60,
+    borderWidth: 1,
+  },
+  addWrapper: {
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 60,
+    height: 60,
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {},
 });
 export default App;
